c5c537ddb1899d146a9478c9b2d71cee0ed58c58
Dmytro Bartoshchuk <dmytro.bartoshchuk@selsey.pl>
2020-11-18 23:33:20 +0100

    ODOO-3 Creating a custom view and report that show backordered inventory items organized by customer and sales order

--- magento_bridge_eb/models/__init__.py
+++ magento_bridge_eb/models/__init__.py
@@ -6,6 +6,7 @@ from . import sale_order
 from . import sale_order_line
 from . import sale_coupon
 from . import stock_picking
+from . import stock_move
 from . import account_move
 from . import custom
 from . import product_template
