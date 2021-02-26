56c339daacadac771842cb7103cd6a72b8bd7387
Dmytro Bartoshchuk <dmytro.bartoshchuk@selsey.pl>
2021-02-26 12:11:15 +0100

    Added context log

--- magento_bridge_eb/models/product_product.py
+++ magento_bridge_eb/models/product_product.py
@@ -56,6 +56,7 @@ class ProductProduct(models.Model):
 
     @api.constrains('standard_price', 'wholesale_markup')
     def mag_update_product_price(self):
+        _logger.info(f"CONTEXT: {self.env.context}")
         is_import = self.env.context.get('import_file') or self.env.context.get('_import_current_module') == '__import__'
         if is_import:
             self.product_tmpl_id.mag_to_update = True
